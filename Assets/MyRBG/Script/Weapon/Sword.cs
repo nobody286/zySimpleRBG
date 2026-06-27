04a8485dca33ff4a824a91517124677554ecb9c3
nobody286 <3198786585@qq.com>
2026-06-27 15:23:59 +0800

    完成主角攻击-挥剑的开发

--- Assets/MyRBG/Script/Weapon/Sword.cs
+++ Assets/MyRBG/Script/Weapon/Sword.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -9,18 +11,24 @@ public class Sword : Weapon
     public Transform PlayerTransform;
     private Animator animator;
     private NavMeshAgent playerAgent;
+    private Collider col;
 
     //攻击相关参数
     [Header("攻击设置")]
     private float attackAnimLength = 1.25f;      //攻击动画长度
     private bool isAttacking = false;
     private float attackTimer;
+    public int atkValue = 50;
     private void Start()
     {
         PlayerTransform = GameObject.FindGameObjectWithTag(TagManager.PLAYER).transform;
         animator = PlayerTransform.GetComponent<Animator>();
         playerAgent = PlayerTransform.GetComponent<NavMeshAgent>();
-     
+        col = GetComponent<Collider>();
+        if(col != null && col.enabled == true)
+        {
+            col.enabled = false;
+        }
     }
     void Update()
     {
@@ -30,7 +38,7 @@ public class Sword : Weapon
     {
         if(other.tag == TagManager.ENEMY)
         {
-           print(other.name);
+            other.GetComponent<EnemyMove>().TakeDamage(atkValue);
         }
     }
     public override void Attack()
@@ -38,22 +46,35 @@ public class Sword : Weapon
         //======================攻击倒计时逻辑=======================
         if (isAttacking)
         {
+     
             attackTimer -= Time.deltaTime;
+            if (attackTimer <= 0.625 && attackTimer >= 0.5f && col.enabled == false)
+            {
+                col.enabled = true;
+            }
+            if(attackTimer <= 0.5 && attackTimer >= 0.25f && col.enabled == true)
+            {
+                col.enabled = false;
+            }
+            if(attackTimer <= 0.25 && attackTimer > 0f && col.enabled == false)
+            {
+                col.enabled = true;
+            }
+
             if (attackTimer <= 0)
             {
+                if(col.enabled == true)
+                    col.enabled = false;
                 isAttacking = false;
                 animator.SetBool("isAttack", false);
             }
-
-            //playerAgent.isStopped = true;  //攻击时停止移动
-            //return;
 
         }
         //playerAgent.isStopped = false; //非攻击时允许移动
 
 
         //========================攻击输入===========================
-        if (Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && !isAttacking)
         {
             isAttacking = true;
             attackTimer = attackAnimLength;
