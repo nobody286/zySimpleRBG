7d2ab2db5bb92893f4a166229abb44b8d0511c54
nobody286 <3198786585@qq.com>
2026-06-28 15:16:45 +0800

    完成代码更新背包ui

---
+++ Assets/MyRBG/Script/UI/InventoryUI/itemUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class itemUI : MonoBehaviour
+{
+    public Image iconImage;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI typeText;
+
+    public void InitItem(Sprite IconSprite , string name , string type)
+    {
+        iconImage.sprite = IconSprite;
+        nameText.text = name;
+        typeText.text = type;
+    }
+}
