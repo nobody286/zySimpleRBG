4c835325acdcb637ba861eda5e395dd0acb36179
nobody286 <3198786585@qq.com>
2026-06-28 16:26:15 +0800

    完成item点击事件

---
+++ Assets/MyRBG/Script/UI/InventoryUI/ItemDetailUI.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemDetailUI : MonoBehaviour
+{
+    public Image iconImage;
+    public TextMeshProUGUI nameText;
+    public TextMeshProUGUI typeText;
+    public TextMeshProUGUI descriptionText;
+    public GameObject propertyGrid;
+    public GameObject propertyTemplate;
+    private void Start()
+    {
+        propertyTemplate.SetActive(false);
+    }
+    public void UpdateItemDetailUI(ItemScriptObject itemSO)
+    {
+        string type = "";
+        switch (itemSO.itemType)
+        {
+            case ItemType.Weapon:
+                type = "武器";
+                break;
+            case ItemType.Consumable:
+                type = "可消耗品";
+                break;
+        }
+        iconImage.sprite = itemSO.icon;
+        nameText.text = itemSO.name;
+        typeText.text = type;
+        descriptionText.text = itemSO.description;
+
+        foreach(Transform child in propertyGrid.transform)
+        {
+            if (child.gameObject.activeSelf)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+        foreach (ItemProperty property in itemSO.propertyList)
+        {
+            string propertyStr = "";
+            string propertyName = "";
+            switch (property.propertyType)
+            {
+                case ItemPropertyType.HPValue:
+                    propertyName = "生命值:";
+                    break;
+                case ItemPropertyType.EnergyValue:
+                    propertyName = "饥饿值:";
+                    break;
+                case ItemPropertyType.MentalValue:
+                    propertyName = "精神值:";
+                    break;
+                case ItemPropertyType.SpeedValue:
+                    propertyName = "速度:";
+                    break;
+                case ItemPropertyType.AttackValue:
+                    propertyName = "攻击力:";
+                    break;
+
+            }
+            propertyStr += propertyName;
+            propertyStr += property.value;
+            GameObject go = GameObject.Instantiate(propertyTemplate);
+            go.SetActive(true);
+            go.transform.parent = propertyGrid.transform;
+            go.transform.Find("Property").GetComponent<TextMeshProUGUI>().text = propertyStr;
+        }                   
+    }
+}
